6dc71271d15136b003f82f1b45177930f381be02
Wanshui Yu <yu.wanshui@utexas.edu>
2016-10-18 18:40:38 -0500

    changes

--- project/app.py
+++ project/app.py
@@ -3,7 +3,12 @@ from flask import Flask, render_template, send_from_directory
  
 app = Flask(__name__)
  
-@app.route('/<string:page_name>/')
+# @app.route('/<string:page_name>/')
+# def render_static(page_name):
+#     print(page_name)
+#     return render_template('%s' % page_name)
+
+@app.route('/<path:page_name>/')
 def render_static(page_name):
     print(page_name)
     return render_template('%s' % page_name)
