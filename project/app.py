a319b570384c779f468434564b347f91edce9498
Wanshui Yu <yu.wanshui@utexas.edu>
2016-10-18 17:48:50 -0500

    inital front end changes

--- project/app.py
+++ project/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, send_from_directory
  
 app = Flask(__name__)
  
-@app.route('/<string:page_name>/')
+@app.route('/<string:page_name>')
 def render_static(page_name):
     print(page_name)
     return render_template('%s' % page_name)
