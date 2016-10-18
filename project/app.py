a5cb52775f9c1b05b0cec62069115d42c2a9230a
EC2 Default User <ec2-user@ip-172-31-21-227.us-west-2.compute.internal>
2016-10-18 01:49:54 +0000

    Updated app.py

--- project/app.py
+++ project/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask, render_template
+ 
+app = Flask(__name__, static_url_path='/templates')
+ 
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s' % page_name)
 
 @app.route('/')
-def index():
-    return "hello cs373f"
-
-@app.route('/books')
-def books():
-    return "This is the books page"
-
+def render_home():
+    return render_template('index.html')
+ 
 if __name__ == "__main__":
     app.run()
