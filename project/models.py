cfb2fca2986668c905d92de945ce0c2e1650b29e
mjvolk <michaelvolk@utexas.edu>
2016-10-19 17:55:05 -0500

    updated models

--- project/models.py
+++ project/models.py
@@ -6,8 +6,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe'
 
 db = SQLAlchemy(app)
 
-tags = db.Table('tags',
+majorToCity = db.Table('majorsToCity',
     db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
+    db.Column('major_id', db.Integer, db.ForeignKey('major.id'))
+)
+
+ethnicityToCity = db.Table('ethnicityToCity',
+    db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
+    db.Column('ethnicity_id', db.Integer, db.ForeignKey('ethnicity.id'))
+)
+
+ethnicityToUniversity = db.Table('ethnicityToUniversity',
+    db.Column('university_id', db.Integer, db.ForeignKey('university.id')),
+    db.Column('ethnicity_id', db.Integer, db.ForeignKey('ethnicity.id'))
+)
+
+majorToUniversity = db.Table('majorToUniversity',
+    db.Column('university_id', db.Integer, db.ForeignKey('university.id')),
     db.Column('major_id', db.Integer, db.ForeignKey('major.id'))
 )
 
@@ -21,26 +36,21 @@ class University(db.Model):
 	numUndergrads = db.Column(db.Integer)
 	costToAttend = db.Column(db.Integer)
 	gradRate = db.Column(db.Float)
-    
-	# todo: relationship
-    location = db.Column(db.String(80), db.ForeignKey('city.id'))
-    
-    publicOrPrivate = db.Column(db.String(80))
+	publicOrPrivate = db.Column(db.String(80))
+	ethnicityList = db.relationship('Ethnicity', secondary=ethnicityToUniversity, backref=db.backref('university', lazy='dynamic'))
+	majorList = db.relationship('Major', secondary=majorToUniversity, backref=db.backref('university', lazy='dynamic'))
 
-    # todo: relationship
-    ethnicityCount = db.Column(db.Integer)
+	def __init__(self, name, numUndergrads, costToAttend, gradRate, location, publicOrPrivate, ethnicityCount):
+		self.name = name
+		self.numUndergrads = numUndergrads
+		self.costToAttend = costToAttend
+		self.gradRate = gradRate
+		self.location = location
+		self.publicOrPrivate = publicOrPrivate
+		self.ethnicityCount = ethnicityCount
 
-    def __init__(self, name, numUndergrads, costToAttend, gradRate, location, publicOrPrivate, ethnicityCount):
-        self.name = name
-        self.numUndergrads = numUndergrads
-        self.costToAttend = costToAttend
-        self.gradRate = gradRate
-        self.location = location
-        self.publicOrPrivate = publicOrPrivate
-        self.ethnicityCount = ethnicityCount
-
-    def __repr__(self):
-        return '<University %r>' % self.name
+	def __repr__(self):
+		return '<University %r>' % self.name
 
 class City(db.Model):
 	'''
@@ -51,17 +61,14 @@ class City(db.Model):
 	name = db.Column(db.String(80))
 	population = db.Column(db.Integer)
 	
-	# todo: relationship
 	universityList = db.relationship('University', backref='city', lazy='dynamic')
-	majorList = db.relationship('Major', backref='major', lazy='dynamic')
+	majorList = db.relationship('Major', secondary=majorToCity, backref=db.backref('city', lazy='dynamic'))
+	ethnicityList = db.relationship('Ethnicity', secondary=ethnicityToCity, backref=db.backref('city', lazy='dynamic'))
 	
 	avgTuition = db.Column(db.Integer)
-    urbanOrRural = db.Column(db.String(80))
+	urbanOrRural = db.Column(db.String(80))
 
-    # todo: relationship
-    ethnicityCount = db.Column(db.Integer)
-
-    def __init__(self, name, population, universityList, majorList, avgTuition, urbanOrRural, ethnicityCount):
+	def __init__(self, name, population, universityList, majorList, avgTuition, urbanOrRural, ethnicityCount):
 		self.name = name
 		self.population = population
 		self.universityList = universityList
@@ -70,8 +77,8 @@ class City(db.Model):
 		self.urbanOrRural = urbanOrRural
 		self.ethnicityCount	= ethnicityCount        
 
-    def __repr__(self):
-        return '<City %r>' % self.name
+	def __repr__(self):
+		return '<City %r>' % self.name
 
 class Major(db.Model):
 	'''
@@ -81,24 +88,19 @@ class Major(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	numUndergrads = db.Column(db.Integer)
-	gradRate = db.Column(db.Float)
-    
-	# todo: relationship
-	universityList = db.relationship('University', backref='city', lazy='dynamic')
-	cityList = db.relationship('City', backref='city', lazy='dynamic')
-	
+	gradRate = db.Column(db.Float)	
 	avgPercentage = db.Column(db.Float)
 	
-    def __init__(self, name, numUndergrads, gradRate, universityList, cityList, avgPercentage):
-        self.name = name
+	def __init__(self, name, numUndergrads, gradRate, universityList, cityList, avgPercentage):
+		self.name = name
 		self.numUndergrads = numUndergrads
 		self.gradRate = gradRate
 		self.universityList = universityList
 		self.cityList = cityList
 		self.avgPercentage = avgPercentage
 
-    def __repr__(self):
-        return '<Major %r>' % self.name
+	def __repr__(self):
+		return '<Major %r>' % self.name
 
 class Ethnicity(db.Model):
 	'''
@@ -108,20 +110,13 @@ class Ethnicity(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	totalCount = db.Column(db.Integer)
-	
-	# todo:	relationship
-	topByCity = db.relationship('City', backref='city', lazy='dynamic')
-	topByUniversity = db.relationship('University', backref='university', lazy='dynamic')
-    
-    # todo:	relationship
-	populationTrend = db.relationship('Ethnicity', backref='ethnicity', lazy='dynamic')
-
-    def __init__(self, name, totalCount, topByCity, topByUniversity, populationTrend):
-        self.name = name
+	    
+	def __init__(self, name, totalCount, topByCity, topByUniversity, populationTrend):
+		self.name = name
 		self.totalCount = totalCount
 		self.topByCity = topByCity
 		self.topByUniversity = topByUniversity
 		self.populationTrend = populationTrend
 
-    def __repr__(self):
-        return '<Ethnicity %r>' % self.name
+	def __repr__(self):
+		return '<Ethnicity %r>' % self.name
