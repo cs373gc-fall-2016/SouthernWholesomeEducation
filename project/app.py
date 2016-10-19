17515c74a1b6497e6754786e596a58610268533c
mxavier6 <matthewxavier95@gmail.com>
2016-10-18 19:00:36 -0500

    Updated files

--- project/app.py
+++ project/app.py
@@ -1,24 +1,23 @@
 import os
 from flask import Flask, render_template, send_from_directory
- 
+
 app = Flask(__name__)
- 
-@app.route('/<string:page_name>/')
+
+
+@app.route('/<string:page_name>')
 def render_static(page_name):
     print(page_name)
     return render_template('%s' % page_name)
+
 
 @app.route('/')
 def render_home():
     return render_template('index.html')
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/books')
-def books():
-    return 'This is books page'
- 
 if __name__ == "__main__":
     app.run()
