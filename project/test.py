5607915b4b30042db8ef8bce135646622f34984e
Brandon Dang <budang@cs.utexas.edu>
2016-10-19 18:52:59 -0500

    Init commit of unit tests

---
+++ project/test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+'''
+Unit tests for models.py
+'''
+
+from unittest import main, TestCase
+from models import *
+
+class test(TestCase):
+	'''
+	Unit tests for models.py
+	'''
+
+	def test_university_1(self):
+		'''
+		Test University class
+		'''
+		pass
+
+	def test_university_2(self):
+		'''
+		Test University class
+		'''
+		pass
+
+	def test_university_3(self):
+		'''
+		Test University class
+		'''
+		pass
+
+	def test_city_1(self):
+		'''
+		Test City class
+		'''
+		pass
+
+	def test_city_2(self):
+		'''
+		Test City class
+		'''
+		pass
+
+	def test_city_3(self):
+		'''
+		Test City class
+		'''
+		pass
+
+	def test_major_1(self):
+		'''
+		Test Major class
+		'''
+		pass
+
+	def test_major_2(self):
+		'''
+		Test Major class
+		'''
+		pass
+
+	def test_major_3(self):
+		'''
+		Test Major class
+		'''
+		pass
+
+	def test_ethnicity_1(self):
+		'''
+		Test Ethnicity class
+		'''
+		pass
+
+	def test_ethnicity_2(self):
+		'''
+		Test Ethnicity class
+		'''
+		pass
+
+	def test_ethnicity_3(self):
+		'''
+		Test Ethnicity class
+		'''
+		pass
