46c5dc8cf21b63171488633a9c3be58fe1609270
mxavier6 <matthewxavier95@gmail.com>
2016-10-18 19:04:12 -0500

    Updated files

--- project/app.py
+++ project/app.py
@@ -15,6 +15,7 @@ def render_home():
     return render_template('/index.html')
 
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
