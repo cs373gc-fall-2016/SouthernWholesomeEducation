db7f18e6ed310b9104153fc18871b84471968da9
Matthew Xavier <mxavier6@cs.utexas.edu>
2016-10-20 15:44:54 -0500

    Added pydoc

--- project/IDB1.py
+++ project/IDB1.py
@@ -3,24 +3,26 @@
 import os
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
-@app.route('/<string:page_name>/')
+@APP.route('/<string:page_name>/')
 def render_static(page_name):
     """Return HTML page stored in templates directory"""
     return render_template('%s' % page_name)
 
-@app.route('/')
+
+@APP.route('/')
 def render_home():
     """Return index.html page when no path is given"""
     return render_template('index.html')
 
-@app.route('/favicon.ico')
+
+@APP.route('/favicon.ico')
 def favicon():
     """Return the favicon.ico"""
-    return send_from_directory(os.path.join(app.root_path, 'static'), \
-        'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(APP.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.run()
+    APP.run()
