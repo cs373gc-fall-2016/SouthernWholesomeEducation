076cf08cac7cb790dd49f9b85f8b9ad797238f13
Matthew Xavier <mxavier6@cs.utexas.edu>
2016-10-20 17:38:21 -0500

    Added formatting

--- project/models.py
+++ project/models.py
@@ -1,134 +1,159 @@
+# pylint: disable=E1101, R0913, C0103, R0201, R0903
+"""Implements the models that will be used in database"""
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe'
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe'
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
-majorToCity = db.Table('majorsToCity',
-    db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
-    db.Column('major_id', db.Integer, db.ForeignKey('major.id'))
-)
+MAJORTOCITY = DB.Table('majorsToCity',
+                       DB.Column('city_id', DB.Integer,
+                                 DB.ForeignKey('city.id')),
+                       DB.Column('major_id', DB.Integer,
+                                 DB.ForeignKey('major.id')))
 
-ethnicityToCity = db.Table('ethnicityToCity',
-    db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
-    db.Column('ethnicity_id', db.Integer, db.ForeignKey('ethnicity.id'))
-)
+ETHNICITYTOCITY = DB.Table('ETHNICITYTOCITY',
+                           DB.Column('city_id', DB.Integer,
+                                     DB.ForeignKey('city.id')),
+                           DB.Column('ethnicity_id', DB.Integer,
+                                     DB.ForeignKey('ethnicity.id')))
 
-ethnicityToUniversity = db.Table('ethnicityToUniversity',
-    db.Column('university_id', db.Integer, db.ForeignKey('university.id')),
-    db.Column('ethnicity_id', db.Integer, db.ForeignKey('ethnicity.id'))
-)
+ETHNICITYTOUNIVERSITY = DB.Table('ETHNICITYTOUNIVERSITY',
+                                 DB.Column('university_id', DB.Integer,
+                                           DB.ForeignKey('university.id')),
+                                 DB.Column('ethnicity_id', DB.Integer,
+                                           DB.ForeignKey('ethnicity.id')))
 
-majorToUniversity = db.Table('majorToUniversity',
-    db.Column('university_id', db.Integer, db.ForeignKey('university.id')),
-    db.Column('major_id', db.Integer, db.ForeignKey('major.id'))
-)
+MAJORTOUNIVERSITY = DB.Table('MAJORTOUNIVERSITY',
+                             DB.Column('university_id', DB.Integer,
+                                       DB.ForeignKey('university.id')),
+                             DB.Column('major_id', DB.Integer,
+                                       DB.ForeignKey('major.id')))
 
-class University(db.Model):
-	'''
-	Class for University pillar. The name, numUndergrads, costToAttend, gradRate, and publicOrPrivate
-	variables will be set from API data and the ethnicityList and majorList will be created from
-	relationships. A city variable will also be included from a backref with the City class.
-	'''
-	__tablename__ = 'UNIVERSITY'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	numUndergrads = db.Column(db.Integer)
-	costToAttend = db.Column(db.Integer)
-	gradRate = db.Column(db.Float)
-	publicOrPrivate = db.Column(db.String(80))
-	ethnicityList = db.relationship('Ethnicity', secondary=ethnicityToUniversity, backref=db.backref('university', lazy='dynamic'))
-	majorList = db.relationship('Major', secondary=majorToUniversity, backref=db.backref('university', lazy='dynamic'))
 
-	def __init__(self, name, numUndergrads, costToAttend, gradRate, publicOrPrivate):
-		self.name = name
-		self.numUndergrads = numUndergrads
-		self.costToAttend = costToAttend
-		self.gradRate = gradRate
-		self.publicOrPrivate = publicOrPrivate
+class University(DB.Model):
+    '''
+    Class for University pillar. The name, numUndergrads, costToAttend, gradRate
+    , and publicOrPrivate variables will be set from API data and the
+    ethnicityList and majorList will be created from relationships. A city
+    variable will also be included from a backref with the City class.
+    '''
+    __tablename__ = 'UNIVERSITY'
+    idnum = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(80))
+    numUndergrads = DB.Column(DB.Integer)
+    costToAttend = DB.Column(DB.Integer)
+    gradRate = DB.Column(DB.Float)
+    publicOrPrivate = DB.Column(DB.String(80))
+    ethnicityList = DB.relationship('Ethnicity', secondary=ETHNICITYTOUNIVERSITY,
+                                    backref=DB.backref('university', lazy='dynamic'))
+    majorList = DB.relationship('Major', secondary=MAJORTOUNIVERSITY,
+                                backref=DB.backref('university', lazy='dynamic'))
 
-	def __repr__(self):
-		return '<University %r>' % self.name
+    def __init__(self, name, numundergrads, costtoattend, gradrate, publicorprivate):
+        self.name = name
+        self.numundergrads = numundergrads
+        self.costtoattend = costtoattend
+        self.gradrate = gradrate
+        self.publicorprivate = publicorprivate
 
-	# These functions create relationships between Universities and Majors and Ethnicities.
-	def addMajor(self, m):
-		majorList.append(m)
+    def __repr__(self):
+        return '<University %r>' % self.name
 
-	def addEthnicity(self, e):
-		ethnicityList.append(e)
+    # These functions create relationships between Universities and Majors and
+    # Ethnicities.
+    def addMajor(self, maj):
+        """Appends new major to majorList"""
+        University.majorList.APPend(maj)
 
-class City(db.Model):
-	'''
-	Class for City pillar. The name and urbanOrRural variables will be set from API data and the 
-	universityList, majorList, and ethnicityList will be created from relationships. The population
-	and avgTuition variables will be aggregated from university data in universityList. 
-	'''
-	__tablename__ = 'CITY'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	population = db.Column(db.Integer)
-	
-	universityList = db.relationship('University', backref='city', lazy='dynamic')
-	majorList = db.relationship('Major', secondary=majorToCity, backref=db.backref('city', lazy='dynamic'))
-	ethnicityList = db.relationship('Ethnicity', secondary=ethnicityToCity, backref=db.backref('city', lazy='dynamic'))
-	
-	avgTuition = db.Column(db.Integer)
-	urbanOrRural = db.Column(db.String(80))
+    def addEthnicity(self, eth):
+        """Appends new ethnicity to ethnicityList"""
+        University.ethnicityList.APPend(eth)
 
-	def __init__(self, name, urbanOrRural):
-		self.name = name
-		self.population = 0
-		self.avgTuition = 0
-		self.urbanOrRural = urbanOrRural
 
-	def __repr__(self):
-		return '<City %r>' % self.name
+class City(DB.Model):
+    '''
+    Class for City pillar. The name and urbanOrRural variables will be set from API data and the
+    universityList, majorList, and ethnicityList will be created from relationships. The population
+    and avgTuition variables will be aggregated from university data in universityList.
+    '''
+    __tablename__ = 'CITY'
+    idNum = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(80))
+    population = DB.Column(DB.Integer)
 
-	# These functions create relationships between Cities and Majors, Ethnicities, and Universities.
-	def addUniversity(self, u):
-		universityList.append(u)
+    universityList = DB.relationship(
+        'University', backref='city', lazy='dynamic')
+    majorList = DB.relationship('Major', secondary=MAJORTOCITY,
+                                backref=DB.backref('city', lazy='dynamic'))
+    ethnicityList = DB.relationship('Ethnicity', secondary=ETHNICITYTOCITY,
+                                    backref=DB.backref('city', lazy='dynamic'))
 
-	def addMajor(self, m):
-		majorList.append(m)
+    avgTuition = DB.Column(DB.Integer)
+    urbanOrRural = DB.Column(DB.String(80))
 
-	def addEthnicity(self, e):
-		ethnicityList.append(e)
+    def __init__(self, name, urbanorrural):
+        self.name = name
+        self.population = 0
+        self.avgtuition = 0
+        self.urbanorrural = urbanorrural
 
-class Major(db.Model):
-	'''
-	Class for Major pillar. The name variable is passed in initially and the numUndergrads, gradRate,
-	and avgPercentage are calculated based on relationships. This class will include a backref table
-	from University (university) and City (city).
-	'''
-	__tablename__ = 'MAJOR'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	numUndergrads = db.Column(db.Integer)
-	gradRate = db.Column(db.Float)	
-	avgPercentage = db.Column(db.Float)
-	
-	def __init__(self, name):
-		self.name = name
+    def __repr__(self):
+        return '<City %r>' % self.name
 
-	def __repr__(self):
-		return '<Major %r>' % self.name
+    # These functions create relationships between Cities and Majors,
+    # Ethnicities, and Universities.
+    def addUniversity(self, uni):
+        """Appends university to universityList"""
+        City.universityList.APPend(uni)
 
-class Ethnicity(db.Model):
-	'''
-	Class for Ethnicity pillar. The name variable is passed in initially and the totalCount variable
-	is calculated based on the University relationship. This class will include a backref table
-	from University (university) and City (city).
-	'''
-	__tablename__ = 'ETHNICITY'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	totalCount = db.Column(db.Integer)
-	    
-	def __init__(self, name):
-		self.name = name
-		self.totalCount = 0
+    def addMajor(self, maj):
+        """Appends major to majorList"""
+        City.majorList.APPend(maj)
 
-	def __repr__(self):
-		return '<Ethnicity %r>' % self.name
+    def addEthnicity(self, eth):
+        """Adds new ethnicity to ethnicityList"""
+        City.ethnicityList.APPend(eth)
+
+
+class Major(DB.Model):
+    '''
+    Class for Major pillar. The name variable is passed in initially and
+    the numUndergrads, gradRate, and avgPercentage are calculated based
+    on relationships. This class will include a backref table
+    from University (university) and City (city).
+    '''
+    __tablename__ = 'MAJOR'
+    idNum = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(80))
+    numUndergrads = DB.Column(DB.Integer)
+    gradRate = DB.Column(DB.Float)
+    avgPercentage = DB.Column(DB.Float)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Major %r>' % self.name
+
+
+class Ethnicity(DB.Model):
+    '''
+    Class for Ethnicity pillar. The name variable is passed in initially and the totalCount variable
+    is calculated based on the University relationship. This class will include a backref table
+    from University (university) and City (city).
+    '''
+    __tablename__ = 'ETHNICITY'
+    idNum = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(80))
+    totalCount = DB.Column(DB.Integer)
+
+    def __init__(self, name):
+        self.name = name
+        self.totalcount = 0
+
+    def __repr__(self):
+        return '<Ethnicity %r>' % self.name
