6e29f56805fcbfe242d8dc3db0ef9f06cd11f8ab
mxavier6 <matthewxavier95@gmail.com>
2016-11-01 01:52:45 -0500

    Updated files

--- project/tests.py
+++ project/tests.py
@@ -57,7 +57,7 @@ class Tests(TestCase):
         university = University.query.filter_by(name='UT',cost_to_attend=0).first()
         university.add_major('Engineering', 50000)
         m = Major.query.filter_by(name='Engineering').first()
-        print(MAJORTOUNIVERSITY.query.filter_by(major_id=2, university_id=2).first().num_students)
+        self.assertEqual(MAJORTOUNIVERSITY.query.filter_by(major_id=m.id_num, university_id=university.id_num).first().num_students, 500)
         # self.assertEqual(num_students, 50000)
 
         # -------------------
