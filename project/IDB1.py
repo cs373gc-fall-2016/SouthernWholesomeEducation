c436d83e4f1c8743c74a5bbc593b9fdb5b570fab
Jiyoung Min <jymin94@gmail.com>
2016-11-03 16:32:51 -0500

    Add an endpoint for handling errors

--- project/IDB1.py
+++ project/IDB1.py
@@ -13,17 +13,26 @@ def render_home():
     """Return index.html page when no path is given"""
     return send_file('templates/index.html')
 
-
+@APP.errorhandler(404)
+@APP.errorhandler(500)
+def error_page(error):
+    return render_template('error.html', status=error)
 
 @APP.route('/api/<string:model_name>/id/<int:id_param>')
 def lookup_model(model_name, id_param):
+    model_name = model_name.title()
     model = get_association(get_model(model_name), id_num=id_param)
-
+    # if model is None:
+        # return error_page()
     return jsonify(results=model.attributes())
 
 @APP.route('/api/<string:model_name>/name/<string:name>')
 def lookup_model_by_name(model_name, name):
+    model_name = model_name.title()
+    name = name.title()
     model = get_association(get_model(model_name), name=name)
+    # if model is None:
+        # return error_page()
 
     return jsonify(results=model.attributes())
 
@@ -47,6 +56,8 @@ def api_models(model_name, page=0):
         models = eval('{0}.query.distinct({0}.name).order_by({0}.{1}{2}).offset(offset).limit(page_size).all()'.format(model_name, sort_by, order))
     else:
         models = eval('{0}.query.offset(offset).limit(page_size).all()'.format(model_name))
+    # if models is None:
+        # return error_page()
 
     list_models = models
     json_list = []
