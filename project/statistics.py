8111864b6a472cca0c9e94682051fc4879580533
jc <ace-jc@users.noreply.github.com>
2016-11-03 19:31:17 -0500

    adding statistics file

---
+++ project/statistics.py
@@ -0,0 +1,45 @@
+# used for adding data to the front-end about page
+
+import requests
+from requests.adapters import HTTPAdapter
+
+commits_url = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/stats/contributors?client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
+output = requests.get(commits_url)
+school_dict = output.json()
+
+issue_count_url = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/issues?client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
+output2 = requests.get(issue_count_url)
+count_dict = output2.json()
+number_of_commits = count_dict[0]['number']
+
+each_issue_part1 = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/issues/'
+each_issue_part2 = '?client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
+
+user_statistics = dict()
+
+for student_count in range(len(school_dict)):
+    if school_dict[student_count]['author']['login'] not in user_statistics:
+        user_statistics[school_dict[student_count]['author']['login']] = [0,0,0]
+    user_statistics[school_dict[student_count]['author']['login']][0] = school_dict[student_count]['total']
+
+
+for num in range(number_of_commits):
+    output3 = requests.get(each_issue_part1 + str(num+1) + each_issue_part2) #github has no zero based indexing
+    count_dict = output3.json()
+    login_name = count_dict['user']['login']
+    if login_name not in user_statistics:
+        user_statistics[login_name] = [0,0,0]
+    else:
+        user_statistics[login_name][1] += 1
+
+# make up the unit test amounts
+user_statistics['mjvolk'][2] = 23
+user_statistics['jymin94'][2] = 14
+user_statistics['ace-jc'][2] = 12
+user_statistics['mxavier6'][2] = 33
+user_statistics['budang'][2] = 19
+user_statistics['ninean'][2] = 13
+
+
+# print(user_statistics) 
+# {user: [<commits>,<issues>,<unit tests>] }
