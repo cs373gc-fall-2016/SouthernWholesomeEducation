1bb85cb59c9d57f67abd75b6360235f1c3c70939
jc <ace-jc@users.noreply.github.com>
2016-11-07 21:16:11 -0600

    making statistics API call faster

--- project/statistics.py
+++ project/statistics.py
@@ -5,30 +5,30 @@ def get_github_stats():
     output = requests.get(commits_url)
     school_dict = output.json()
 
-    issue_count_url = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/issues?state=closed&client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
+    issue_count_url = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/issues?state=all&per_page=100&client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
     output2 = requests.get(issue_count_url)
     count_dict = output2.json()
     number_of_commits = count_dict[0]['number']
 
-    each_issue_part1 = 'https://api.github.com/repos/cs373gc-fall-2016/SouthernWholesomeEducation/issues/'
-    each_issue_part2 = '?client_id=acfa1c2e958731bb1f90&client_secret=a6c4060bd74b28cedb5e02b0a0068750d05af16f'
-
     user_statistics = dict()
+
+    print('first for loop starting')
 
     for student_count in range(len(school_dict)):
         if school_dict[student_count]['author']['login'] not in user_statistics:
             user_statistics[school_dict[student_count]['author']['login']] = {'commits': 0, 'issues': 0, 'unit_tests': 0}
         user_statistics[school_dict[student_count]['author']['login']]['commits'] = school_dict[student_count]['total']
 
+    print('end of first for loop... beginning of second for loop')
 
-    for num in range(number_of_commits):
-        output3 = requests.get(each_issue_part1 + str(num+1) + each_issue_part2) #github has no zero based indexing
-        count_dict = output3.json()
-        login_name = count_dict['user']['login']
+    for num in range(len(count_dict)):
+        login_name = count_dict[num]['user']['login']
         if login_name not in user_statistics:
             user_statistics[login_name] = {}
         else:
             user_statistics[login_name]['issues'] += 1
+
+    print('end of second for loop adding unit tests')
 
     user_statistics['mjvolk']['unit_tests'] = 23
     user_statistics['jymin94']['unit_tests'] = 14
@@ -37,6 +37,7 @@ def get_github_stats():
     user_statistics['budang']['unit_tests'] = 19
     user_statistics['ninean']['unit_tests'] = 13
 
+    print('end of unit test starting third for loop')
 
     total_statistics = {'commits': 0, 'issues': 0, 'unit_tests': 0}
     for team_member in user_statistics:
@@ -44,7 +45,8 @@ def get_github_stats():
         total_statistics['issues'] += int(user_statistics[team_member]['issues'])
         total_statistics['unit_tests'] += int(user_statistics[team_member]['unit_tests'])
 
+    print('finished with all of the loops ')
+
     # http://localhost:5000/githubstats
     final_json = {'user_stats' : user_statistics, 'total_stats':total_statistics}
-    # print(final_json)
     return final_json
