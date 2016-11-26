3c2851b1da7a3381ee8fcc470cdc47e0d135b02e
jc <ace-jc@users.noreply.github.com>
2016-11-26 03:09:59 -0600

    updating visualization page

--- project/opensourcery.py
+++ project/opensourcery.py
@@ -3,20 +3,26 @@ from requests.adapters import HTTPAdapter
 from models import *
 import json
 
+# global variables containing id to name mappings
+contributors_mapping = dict()
+language_mapping = dict()
+project_mapping = dict()
+
 
 def api_call():
     # setting up url to collect all school codes
-    projects_url = 'http://opensourcery.me/api/projects/?start=0&end=4' # 0-6676
-    companies_url = 'http://opensourcery.me/api/companies/?start=0&end=3' # 0-299
-    contributors_url = 'http://opensourcery.me/api/contributors/?start=0&end=10' #0-35420
-    languages_url = 'http://opensourcery.me/api/languages/?start=0&end=10' # 0-95
+    projects_url = 'http://opensourcery.me/api/projects/?start=0&end=20' # 0-6676 is max
+    companies_url = 'http://opensourcery.me/api/companies/?start=0&end=4' # 0-299 is max
+    contributors_url = 'http://opensourcery.me/api/contributors/?start=0&end=20' #0-35420 is max
+    languages_url = 'http://opensourcery.me/api/languages/?start=0&end=20' # 0-95 is max
 
-    
     projects = setup_projects(projects_url)
+    get_project_names()
     print('finished projects')
     companies = setup_companies(companies_url)
     print('finished companies')
     contributors = setup_contributors(contributors_url)
+    get_contributor_names()
     print('finished contributors')
     languages = setup_languages(languages_url)
     print('finished languages')
@@ -29,29 +35,33 @@ def api_call():
     final_list.extend(languages)
     print('finised lists')
 
-    print(final_list)
-    print('#########################################################')
 
-    name_list = list()
     for item in final_list:
-        name_list.append(item['name'])
-
-    
-    for item_list in final_list:
-        imports_list = item_list['imports']
-        temp_list = []
-        # print(imports_list)
-        for individual_item in imports_list:
-            if individual_item in name_list:
-                temp_list.append(individual_item)
-        item_list['imports'] = temp_list
-
+        # add the contributor names instead of id
+        if item['target'][0] == 'c':
+            try:
+                item['target'] = contributors_mapping[item['target']]
+            except KeyError:
+                pass # missing contributor
+        # add the language names instead of id
+        if item['target'][0] == 'l':
+            try:
+                item['target'] = language_mapping[item['target']]
+            except KeyError:
+                pass # missing language
+        # add the project names instead of id
+        if item['target'][0] == 'p':
+            try:
+                item['target'] = project_mapping[item['target']]
+            except KeyError:
+                pass # missing language
 
     print(final_list)
     print('#########################################################')
 
-    with open('flare-import.json', 'w') as fout:
-        json.dump(final_list, fout)
+
+    # with open('flare-import.json', 'w') as fout:
+    #     json.dump(final_list, fout)
 
 
 def setup_projects(url):
@@ -60,19 +70,20 @@ def setup_projects(url):
     temp_json = response.json()
     for item in range(0, len(temp_json)):
         try:
+            # [{source: "projects", target: "Contributors", type: "licensing"}, ...]
             temp_dict = dict()
-            temp_list_contrib = list()
-            project_id = temp_json[item]['id']
-            temp_proj_id = 'p' + str(project_id)
-            for contrib in temp_json[item]['contributor_ids']:
-                temp_val = 'c' + str(contrib)
-                temp_list_contrib.append(temp_val)
-            for language in temp_json[item]['language_ids']:
-                temp_val = 'l' + str(language)
-                temp_list_contrib.append(temp_val)
-            temp_dict['name'] = temp_proj_id
-            temp_dict['imports'] = temp_list_contrib
-            temp_projects.append(temp_dict)
+            temp_dict['source'] = temp_json[item]['name']
+            for indv_contributor in temp_json[item]['contributor_ids']:
+                if indv_contributor < 300:
+                    temp_str = 'c' + str(indv_contributor)
+                    temp_dict['target'] = temp_str
+                    temp_dict['type'] = 'proj_contrib'
+                    temp_projects.append(temp_dict.copy())
+            for indv_lang in temp_json[item]['language_ids']:
+                temp_str = 'l' + str(indv_lang)
+                temp_dict['target'] = temp_str
+                temp_dict['type'] = 'proj_lang'
+                temp_projects.append(temp_dict.copy())
         except KeyError:
             pass # no key
         except NameError:
@@ -87,22 +98,25 @@ def setup_companies(url):
     temp_json = response.json()
     for item in range(0, len(temp_json)):
         try:
+            # [{source: "companies", target: "Project", type: "licensing"}, ...]
             temp_dict = dict()
-            temp_list_contrib = list()
-            project_id = temp_json[item]['id']
-            temp_proj_id = 'i' + str(project_id) # c was used i for "inc."
-            for project in temp_json[item]['project_ids']:
-                temp_val = 'p' + str(project)
-                temp_list_contrib.append(temp_val)
-            temp_dict['name'] = temp_proj_id
-            temp_dict['imports'] = temp_list_contrib
-            temp_projects.append(temp_dict)
+            temp_dict['source'] = temp_json[item]['name']
+            for indv_project in temp_json[item]['project_ids']:
+                temp_str = 'p' + str(indv_project)
+                temp_dict['target'] = temp_str
+                temp_dict['type'] = 'comp_proj'
+                temp_projects.append(temp_dict.copy())
+            for indv_contributor in temp_json[item]['member_ids']:
+                if indv_contributor < 300:
+                    temp_str = 'c' + str(indv_contributor)
+                    temp_dict['target'] = temp_str
+                    temp_dict['type'] = 'comp_contrib'
+                    temp_projects.append(temp_dict.copy())
         except KeyError:
             pass # no key
         except NameError:
             pass # incorrect name
     return temp_projects
-
 
 
 def setup_contributors(url):
@@ -111,16 +125,14 @@ def setup_contributors(url):
     temp_json = response.json()
     for item in range(0, len(temp_json)):
         try:
+            # [{source: "contributors", target: "Contributors", type: "licensing"}, ...]
             temp_dict = dict()
-            temp_list_contrib = list()
-            project_id = temp_json[item]['id']
-            temp_proj_id = 'c' + str(project_id)
-            for project in temp_json[item]['project_ids']:
-                temp_val = 'p' + str(project)
-                temp_list_contrib.append(temp_val)
-            temp_dict['name'] = temp_proj_id
-            temp_dict['imports'] = temp_list_contrib
-            temp_projects.append(temp_dict)
+            temp_dict['source'] = temp_json[item]['username']
+            for indv_contributor in temp_json[item]['project_ids']:
+                temp_str = 'p' + str(indv_contributor)
+                temp_dict['target'] = temp_str
+                temp_dict['type'] = 'contrib_proj'
+                temp_projects.append(temp_dict.copy())
         except KeyError:
             pass # no key
         except NameError:
@@ -135,21 +147,49 @@ def setup_languages(url):
     temp_json = response.json()
     for item in range(0, len(temp_json)):
         try:
+            # [{source: "languages", target: "Contributors", type: "licensing"}, ...]
             temp_dict = dict()
-            temp_list_contrib = list()
-            project_id = temp_json[item]['id']
-            temp_proj_id = 'l' + str(project_id)
-            for project in temp_json[item]['project_ids']:
-                temp_val = 'p' + str(project)
-                temp_list_contrib.append(temp_val)
-            temp_dict['name'] = temp_proj_id
-            temp_dict['imports'] = temp_list_contrib
-            temp_projects.append(temp_dict)
+            temp_dict['source'] = temp_json[item]['name']
+            language_mapping['l' + str(temp_json[item]['id'])] = temp_json[item]['name']
+            for indv_contributor in temp_json[item]['project_ids']:
+                if indv_contributor < 100:
+                    temp_str = 'p' + str(indv_contributor)
+                    temp_dict['target'] = temp_str
+                    temp_dict['type'] = 'lang_proj'
+                    temp_projects.append(temp_dict.copy())
         except KeyError:
             pass # no key
         except NameError:
             pass # incorrect name
     return temp_projects
+
+
+
+def get_contributor_names():
+    url = 'http://opensourcery.me/api/contributors/?start=0&end=35420' #0-35420 is max
+    response = requests.get(url)
+    temp_json = response.json()
+    for item in range(0, len(temp_json)):
+        try:
+            contributors_mapping['c' + str(temp_json[item]['id'])] = temp_json[item]['username']
+        except KeyError:
+            pass # no key
+        except NameError:
+            pass # incorrect name
+
+
+def get_project_names():
+    url = 'http://opensourcery.me/api/projects/?start=0&end=6676' # 0-6676 is max
+    response = requests.get(url)
+    temp_json = response.json()
+    for item in range(0, len(temp_json)):
+        try:
+            project_mapping['p' + str(temp_json[item]['id'])] = temp_json[item]['name']            
+        except KeyError:
+            pass # no key
+        except NameError:
+            pass # incorrect name
+
 
 
 if __name__ == '__main__':
